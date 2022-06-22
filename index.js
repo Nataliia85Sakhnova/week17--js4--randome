45d9a687ebfdee53d529ff1d01aeda00cf3372e0
Razhatawka <nataliia85sakhnova@gmail.com>
2022-06-22 20:26:04 +0300

    a

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
-// 4. Сделайте генератор 10 случайных чисел (по нажатию на кнопку) в диапазоне от -10 до 10
-// найдите среди них минимальное, максимальное и среднее арифметическое, а также сумму и произведение всех этих чисел.
-// *В этой задаче вам понадобятся массивы и циклы.*
-// Пример: Сгенерировали: -9 1 8 -2 3 2 0 1 -5 7* Мин:-9, Макс:8, Ср.арифм:0.6, Сумма чисел:6, Произведение чисел:0
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  }
+console.log(getRandomIntInclusive(-10, 10));
 
-// Math.random() - генерация случайного числа от 0 до 1
-// Math.ceil(параметр) – округление в большую сторону;
-// Math.floor(параметр) – округление в меньшую сторону;
-// Math.round(параметр) – математическое округление, т.е. когда дробная часть числа больше или равна 0.5, то оно округляется в большую сторону, а иначе в меньшую сторону;
-// Math.trunc(параметр) – отбрасывает дробную часть числа.
-// Возвращения максимального значения из чисел, указанных в качестве параметров:`Math.max(параметр_1,параметр_2,...)`
-// let max = Math.max(-65,8,6,1,90,-4,2);
-// document.write("Максимальное число равно " + max.toString());
+document.getElementById('button').onclick = () => {
+    document.getElementById('container').innerText = (getRandomIntInclusive(-10, 10));
+}; //кнопка+функция
 
-console.log(Math.round(Math.random() * 10));
+// let arr = [];
+// arr.push(Math.round(Math.random() * 10));
+// arr.push(Math.round(Math.random() * 10));
+// arr.push(Math.round(Math.random() * 10));
+// arr.push(Math.round(Math.random() * 10));
+// arr.push(Math.round(Math.random() * 10));
+// arr.push(Math.round(Math.random() * 10));
+// arr.push(Math.round(Math.random() * 10));
+// arr.push(Math.round(Math.random() * 10));
+// arr.push(Math.round(Math.random() * 10));
+// arr.push(Math.round(Math.random() * 10));
+// console.log(arr);
 
-let arr = [];
-arr.push(Math.round(Math.random() * 10));
-arr.push(Math.round(Math.random() * 10));
-arr.push(Math.round(Math.random() * 10));
-arr.push(Math.round(Math.random() * 10));
-arr.push(Math.round(Math.random() * 10));
-arr.push(Math.round(Math.random() * 10));
-arr.push(Math.round(Math.random() * 10));
-arr.push(Math.round(Math.random() * 10));
-arr.push(Math.round(Math.random() * 10));
-arr.push(Math.round(Math.random() * 10));
-console.log(arr);
-
-let i = 0;
-while(i <= 10 && i >= -10)
-while (arr.length < 11) { // выводит 0, затем 1, затем 2
-arr.push(Math.round(Math.random() * 10))
-arr.push(Math.round(Math.random() * 10))
+// let i = 0;
+// while(i <= 10 && i >= -10)
+// while (arr.length < 11) { // выводит 0, затем 1, затем 2
+// arr.push(Math.round(Math.random() * 10))
+// arr.push(Math.round(Math.random() * 10))
 
 // начало	let i = 0	Выполняется один раз при входе в цикл
 // условие	i < 3	Проверяется перед каждой итерацией цикла. Если оно вычислится в false, цикл остановится.
@@ -45,9 +40,12 @@ arr.push(Math.round(Math.random() * 10))
 // → (Если *условие* == true → Выполнить *тело*, Выполнить *шаг*)
 // → ...
 
-for (let i = 0; i < 10; Math.round(Math.random() * 10)) {
-    if (arr.length != 10) {
-        arr.push(i);
-        console.log(arr)
-    }
-}
+// for (let i = 0; i < 10; i++) {
+//     if (arr.length != 10) {
+
+//         Math.round(Math.random() * 10)
+
+//         arr.push(i);
+//         console.log(arr)
+//     }
+// }
