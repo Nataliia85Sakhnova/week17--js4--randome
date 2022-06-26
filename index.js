c938dcf206c69ab4ee715e5098c4c9e1e7937099
Razhatawka <nataliia85sakhnova@gmail.com>
2022-06-26 09:57:05 +0300

    c

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+    return Math.floor(Math.random() * (max - min + 1)) + min; //Создается рандомное число, макс-мин вкл
 }
 console.log(getRandomIntInclusive(-10, 10));
 
@@ -11,25 +11,43 @@ const arrMaker = () => {
         arr.push(getRandomIntInclusive(-10, 10));
     }
     console.log(arr);
-}
+    return arr;
+} //Создается массив из 10 чисел
 
 const Max = () => {
-    let max = Math.max(arrMaker);
+    let max = Math.max.apply(arrMaker);
+    console.log(max);
+    return max;
+}
+
+const Min = () => {
+    let min = Math.min.apply(arrMaker);
     console.log(max);
 }
 
+const Average = () => {
+    let aver = arrMaker
+}
+
+const counter = () => {
+    document.getElementById('container').innerHTML = "";
+    const NewArr = arrMaker();
+    const par = document.createElement("p"); //"p" - параграф
+    par.innerText = `Случайные числа: ${NewArr}`;
+    document.getElementById('container').appendChild(par);
+
+    const NewMax = Max();
+    par.innerText = `Максимум: ${NewMax}`;
+    document.getElementById('container').appendChild(par);
+    
+    // const NewMin = Min();
+    // par.innerText = `Минимум: ${NewMin}`;
+    // document.getElementById('container').appendChild(par);
+
+    // const NewAver = Average();
+    // par.innerText = `Среднее арифметическое: ${NewMin}`;
+    // document.getElementById('container').appendChild(par);
+}
 
 
-
-
-document.getElementById('button').onclick = () => {
-    document.getElementById('container').innerText = (getRandomIntInclusive(-10, 10));
-}; //кнопка+функция
-
-document.getElementById('button1').onclick = () => {
-    document.getElementById('container1').innerText = (arrMaker());
-}; //кнопка1+функция1
-
-document.getElementById('Max-button').onclick = () => {
-    document.getElementById('Max-container').innerText = (Max());
-}; //кнопка1+функция1
+document.getElementById('button').onclick = () => counter(); //кнопка+функция
